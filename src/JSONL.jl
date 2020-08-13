dac8251ed5a35724a20a177cc31b97353aef4bb2
Daniel Winkler <danielw2904@disroot.org>
2020-08-13 13:00:22 +0200

    added Tables dependency and removed promotecols argument (was unused)

--- src/JSONL.jl
+++ src/JSONL.jl
@@ -1,7 +1,8 @@
 module JSONL
 
 import JSON3, 
-    Mmap
+    Mmap,
+    Tables
 
 export readfile,
     writefile
@@ -16,12 +17,11 @@ Read (parts of) a JSONLines file.
 * `file`: Path to JSONLines file
 * Keyword Arguments:
     * `structtype = nothing`: StructType passed to JSON3.read for each row of the file
-    * `promotecols::Bool = false`: Promote columns to Float64, Int64 or String
     * `nrows = nothing`: Number of rows to load
     * `skip = nothing`: Number of rows to skip before loading
     * `usemmap::Bool = (nrows !== nothing || skip !=nothing)`: Memory map file (required for nrows and skip)
 """
-function readfile(file; structtype = nothing, promotecols::Bool = false, nrows = nothing, skip = nothing, usemmap::Bool = (nrows !== nothing || skip !== nothing))
+function readfile(file; structtype = nothing, nrows = nothing, skip = nothing, usemmap::Bool = (nrows !== nothing || skip !== nothing))
     ff = getfile(file, nrows, skip, usemmap)
     length(ff) == 0 && return JSON3.Object[]
     if isnothing(structtype)
